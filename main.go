e2a1f60ebcd9fab1b2bdd5a0a04f05ed266804bf
Kira <k-korni1ov@yandex.ru>
2025-12-13 13:41:11 +0400

    feat: wildcard segments route patterns in prectice + extend use with subtree (restricting etc) + exapmle remainer wildcard

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +14,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // add a snippetView  handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	//Extract the value of the id wildcard from the request using r.PathValue()
+	//and try to convert it to an integer using the strconv.Atoi() function. If
+	// return a 404 page not found response.
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
+
+}
+func snippetRemanderView(w http.ResponseWriter, r *http.Request) {
+	//Extract the value of the id wildcard from the request using r.PathValue()
+	//and try to convert it to an integer using the strconv.Atoi() function. If
+	// return a 404 page not found response.
+	id := r.PathValue("path")
+
+	msg := fmt.Sprint("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
 
 }
 
@@ -25,7 +46,11 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/", snippetView)
+	mux.HandleFunc("/snippet/view/{path}/", snippetView)
+	mux.HandleFunc("/snippet/view/remainder/{path...}", snippetRemanderView)
+	//example remainer wildcards is msg :
+	//http://localhost:4000/snippet/view/remainder/sdf/sdfs/sdf/2/34/2
+	// Display a specific snippet with ID %d...sdf/sdfs/sdf/2/34/2
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on : 4000")
