f41e21e20f32449afa09237b144fae04701bee88
Kira <k-korni1ov@yandex.ru>
2025-12-13 12:39:32 +0400

    feat: restrictng subtree and did't use default mux that behavoir could be like a magic

--- main.go
+++ main.go
@@ -10,9 +10,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// add a snippetView  handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on : 4000")
 	err := http.ListenAndServe(":4000", mux)
