ba5f6e9f77a5dccf72d5ea0116ff742f0b47aee1
Kira <k-korni1ov@yandex.ru>
2025-12-13 20:58:01 +0400

    feat: completed Customizing responses

--- main.go
+++ main.go
@@ -8,7 +8,26 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Server", "Go")     //need type before WriteHEader
+	////Manipulating the header map
+	//Set a new cache-control header . If an existing "Cache-Control" header exists
+	// it will be overwritten.
+	w.Header().Set("Cache-Control", "public, max-age=31536000 ")
+
+	//In contrast, the Add() method appends a new "Cache-Control" header and can
+	// be called multiple times.
+	w.Header().Add("Cache-Control", "public")
+	w.Header().Add("Cache-Control", "max-age=31536000")
+
+	//Delete all values for the "Cacha-Control" header.
+	w.Header().Del("Cache-Control")
+
+	//Retrieve the first value for the "Cache-Control" header.
+	w.Header().Get("Cache-Control")
+	//Retrieve a slice of all values for the "Cache-Control" header.
+	w.Header().Values("Cache-Control")
+	//below comment oldCode
+	//w.Header().Add("Server", "Go")     //need type before WriteHEader
+	w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
 	w.WriteHeader(http.StatusAccepted) //need type defore Write
 	w.Write([]byte("HEllo from snippetbox"))
 
@@ -24,14 +43,15 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 
 }
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(`{"name":"Display a form for creating a new snippet..."}`))
 
 }
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
