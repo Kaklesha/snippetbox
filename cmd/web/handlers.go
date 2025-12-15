d29c4d081fdbc4278feb5971656b84b1465ce31c
Kira <k-korni1ov@yandex.ru>
2025-12-15 12:18:41 +0400

    feat: html home page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -28,8 +30,29 @@ func home(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Add("Server", "Go")     //need type before WriteHEader
 	w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
 	w.WriteHeader(http.StatusAccepted) //need type defore Write
-	w.Write([]byte("HEllo from snippetbox"))
+	////w.Write([]byte("HEllo from snippetbox"))
 
+	//Use the template.ParseFiles() function to read the template file into a
+	//template set. If there's an error , we log the detailed error message, use
+	//the http.Error() function to send an Internal Server Error response to the
+	//  user , and then return from the handler so no subsequent code is executed.
+
+	ts, err := template.ParseFiles("./ui/html/pages/home.html")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Then we use the Execute() method on the template set to write the
+	// template content as the response body. The last parameter to Execute()
+	// represents any dynamic data that we want to pass in, which for now we'll
+	//leave as nil
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 // add a snippetView  handler function.
