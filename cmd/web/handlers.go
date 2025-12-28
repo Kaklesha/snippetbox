7864f5478c4f875ce13d6d1fb96f7c72d44097cf
Kira <k-korni1ov@yandex.ru>
2025-12-28 14:37:00 +0400

    feat: added placeholder of userAuthFuncs associated with it

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -129,3 +129,20 @@ func (app *application) snippetTransact(w http.ResponseWriter, r *http.Request) 
 	w.Write([]byte(`{"transaction":"Display about successful..."}`))
 
 }
+
+// AuthUser
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a form for signing up a new user...")
+}
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new user...")
+}
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a form for logging in a user...")
+}
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Authenticate and login the user...")
+}
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Logout the user...")
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,6 +14,14 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /snippet/view/{id}", app.sessionManager.LoadAndSave(http.HandlerFunc(app.snippetView)))
 	mux.Handle("GET /snippet/create", app.sessionManager.LoadAndSave(http.HandlerFunc(app.snippetCreate)))
 	mux.Handle("POST /snippet/create", app.sessionManager.LoadAndSave(http.HandlerFunc(app.snippetCreatePost)))
+
+	// Add the five new routes, all of which use our 'dynamic' middleware chain.
+	mux.Handle("GET /user/signup", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userSignup)))
+	mux.Handle("POST /user/signup", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userSignupPost)))
+	mux.Handle("GET /user/login", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userLogin)))
+	mux.Handle("POST /user/login", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userLoginPost)))
+	mux.Handle("POST /user/logout", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userLogoutPost)))
+
 	// Create a middleware chain containing our 'standard' middleware
 	// which will be used for every request our application receives.
 	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
